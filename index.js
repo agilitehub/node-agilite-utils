cd9d839766c034ca336f567b1e54dc6a5e16f6eb
John Jardin <john@agilite.io>
2019-10-18 08:36:09 +0200

    Added dotenv and uuid modules to Utils as well as fixed axios module

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const Utils = {
+  dotEnv: require('dotenv'),
   enumsTypeDetect: require('./lib/enums-type-detect'),
   typeDetect: require('type-detect'),
   nanoPerformance: require('nano-performance'),
   dateFNS: require('date-fns'),
   genericPool: require('generic-pool'),
   servicePool: require('service-pool'),
-  axios: require('axios'),
+  axios: require('axios').default,
   bcrypt: require('./lib/bcrypt'),
-  jwtSimple: require('./lib/jwt-simple')
+  jwtSimple: require('./lib/jwt-simple'),
+  uuid: require('uuid')
 }
 
 module.exports = Utils
